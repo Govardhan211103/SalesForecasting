93b0e96a8c0c3012c4803769313f7ec366fe43a2
Govardhan211103 <vgovardhanvarma.vh@gmail.com>
2023-06-21 01:06:47 +0530

    Commit5 src.files

---
+++ src/exception.py
@@ -0,0 +1,27 @@
+import sys
+import logging
+import logger
+
+def error_msg_detail(error,error_detail:sys):
+    exc_type,exc_instance,exc_traceback=error_detail.exc_info()
+    file_name=exc_traceback.tb_frame.f_code.co_filename
+    error_msg="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+            file_name,
+            exc_traceback.tb_lineno,
+            str(error)
+            )
+    return error_msg
+
+class CustomException(Exception):
+    def __init__(self,error_msg,error_detail:sys):
+        super().__init__(error_msg)
+        self.error_msg=error_msg_detail(error_msg,error_detail=error_detail)
+    def __str__(self):
+        return self.error_msg
+
+if __name__=='__main__':
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Excpetion raised")
+        raise CustomException(e,sys)
---
+++ src/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%d_%m_%Y-%H_%M_%S')}.log"
+
+log_path=os.path.join(os.getcwd(),LOG_FILE)
+os.makedirs(log_path,exist_ok=True)
+
+logging.basicConfig(
+    filename=os.path.join(log_path,LOG_FILE),
+    level=logging.INFO,
+    format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
+
